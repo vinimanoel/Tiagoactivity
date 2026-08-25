f2ca821a7cea0678fb24b83eb180871c3b10a144
Italo Javosky <silva.camilo@escolar.ifrn.edu.br>
2026-08-25 07:43:35 -0300

    script
    player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,46 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public float velocidade = 5f;
+
+    private Rigidbody rb;
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Vector2 movimento = Vector2.zero;
+
+        if (Keyboard.current.wKey.isPressed)
+            movimento.y += 1;
+
+        if (Keyboard.current.sKey.isPressed)
+            movimento.y -= 1;
+
+        if (Keyboard.current.aKey.isPressed)
+            movimento.x -= 1;
+
+        if (Keyboard.current.dKey.isPressed)
+            movimento.x += 1;
+
+        movimento = movimento.normalized;
+
+        Vector3 direcao = new Vector3(
+            movimento.x,
+            0,
+            movimento.y
+        );
+
+        Vector3 velocidadeAtual = rb.linearVelocity;
+
+        velocidadeAtual.x = direcao.x * velocidade;
+        velocidadeAtual.z = direcao.z * velocidade;
+
+        rb.linearVelocity = velocidadeAtual;
     }
 }
